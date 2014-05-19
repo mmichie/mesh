52e29245a59de8fd84ceba8450875cff3f7d25cc
Matt Michie <mmichie@twitter.com>
2014-05-19 13:50:48 -0700

    Ignore builtin commands that overlap system commands, increase logging

--- mesh.py
+++ mesh.py
@@ -2,7 +2,6 @@
 
 import logging
 import os
-import shutil
 import sys
 import readline
 import traceback
@@ -10,11 +9,14 @@ import traceback
 from command import Command
 
 def setup():
+    FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+
     mesh_dir = os.path.expanduser('~') + '/.mesh/'
     if not os.path.exists(mesh_dir):
         os.mkdir(mesh_dir, mode=0o700)
     
-    logging.basicConfig(filename=mesh_dir + 'mesh.log', level=logging.DEBUG)
+    logging.basicConfig(filename=mesh_dir + 'mesh.log', level=logging.DEBUG, format=FORMAT)
+
     readline.parse_and_bind('tab: complete')
     readline.parse_and_bind('set editing-mode vi')
     logging.info('Set readline modes')
@@ -30,13 +32,14 @@ def record_command(command):
     logging.info('Recording %s' % command)
 
 def run_builtin(command):
-    if shutil.which(command.command[0]):
-        os.system(command.text)
-    elif command.command[0] == 'cd':
+    if command.command[0] == 'cd':
         os.chdir(command.command[1])
+        logging.debug('Built in chdir to: %s' % command.command[1])
     elif command.command[0] == 'pwd':
         print(os.getcwd())
+        logging.debug('Built in pwd')
     elif command.command[0] == 'exit':
+        logging.debug('Built in exit')
         sys.exit()
 
 if __name__ == "__main__":
@@ -61,4 +64,5 @@ if __name__ == "__main__":
             print('')
             break
         except:
+            logging.exception('Uncaught exception')
             traceback.print_exc()
