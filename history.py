858b02721410ffdff0dbad3a7d85ef32da0ec85f
Matt Michie <mmichie@twitter.com>
2014-05-24 13:57:55 -0700

    Start adding more detail into history sql tables

--- history.py
+++ history.py
@@ -15,14 +15,38 @@ class BaseHistory:
 
 class SQLiteHistory(BaseHistory):
 
+    """
+    Inspired by ideas and code from: 
+    https://code.google.com/p/advanced-shell-history
+    Carl Anderson - 2012 - Apache License, Version 2.0 
+    """
+
     def __init__(self):
+        command_create_sql = \
+            '''CREATE TABLE IF NOT EXISTS commands(
+                    id integer primary key autoincrement,
+                    session_id integer not null,
+                    shell_level integer not null,
+                    command_no integer,
+                    tty varchar(20) not null,
+                    euid int(16) not null,
+                    cwd varchar(256) not null,
+                    rval int(5) not null,
+                    start_time integer not null,
+                    end_time integer not null,
+                    duration integer not null,
+                    pipe_cnt int(3),
+                    pipe_vals varchar(80),
+                    command varchar(1000) not null,
+                    UNIQUE(session_id, command_no)
+              )'''
 
         db_filename = os.path.expanduser('~') + '/.mesh/history.sqlite'
         self.conn = sqlite3.connect(db_filename, detect_types=sqlite3.PARSE_DECLTYPES)
 
         with self.conn:
             c = self.conn.cursor()
-            c.execute('CREATE TABLE IF NOT EXISTS history(datetime text, statement text)')
+            c.execute(command_create_sql)
 
     def insert(self, statement):
         with self.conn:
